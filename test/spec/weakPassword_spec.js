8317aa7829eba9598b2007ae5a8c47ccd26d89b9
peterhendrick <peterh@hdslabs.com>
2015-03-21 14:09:41 -0600

    added

--- test/spec/weakPassword_spec.js
+++ test/spec/weakPassword_spec.js
@@ -1,5 +1,8 @@
+// var repo = require('../elementRepo.js');
+
 describe('user', function (){
   beforeEach(function() {
+    // browser.get('https://meane2e.herokuapp.com/#!/')
     browser.get('http://localhost:3000')
   });
   afterEach(function() {
@@ -9,6 +12,7 @@ describe('user', function (){
   it('should be able to sign up and recieve a message', function () {
     browser.waitForAngular();
     element(by.id('signUp')).click();
+    
     element(by.id('firstName')).sendKeys('user');
     element(by.id('lastName')).sendKeys('name');
     element(by.id('email')).sendKeys('usera@name.com');
@@ -18,3 +22,24 @@ describe('user', function (){
     expect(element(by.binding('error')).isPresent()).toBeTruthy();
   });
 });
+// describe('user', function (){
+//   beforeEach(function() {
+//     // browser.get('https://meane2e.herokuapp.com/#!/')
+//     browser.get('http://localhost:3000')
+//   });
+//   afterEach(function() {
+//
+//   });
+//
+//   it('should be able to sign up and recieve a message', function () {
+//     browser.waitForAngular();
+//     repo.signUpHeaderButton.click();
+//     repo.firstName.sendKeys('user');
+//     repo.lastName.sendKeys('name');
+//     repo.email.sendKeys('usera@name.com');
+//     element(by.id('username')).sendKeys('username');
+//     element(by.id('password')).sendKeys('pass');
+//     element(by.id('submitButton')).click();
+//     expect(element(by.binding('error')).isPresent()).toBeTruthy();
+//   });
+// });
